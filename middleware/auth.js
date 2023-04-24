843189b637f2fe5c66f1f702bc2726f6ef52a1dc
shutter <6331335921@student.chula.ac.th>
2023-04-24 13:37:38 +0700

    authentication

---
+++ middleware/auth.js
@@ -0,0 +1,37 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+//Protect Routes
+exports.protect = async (req, res, next) => {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    token = req.headers.authorization.split(" ")[1];
+  }
+
+  //Make sure token exists
+  if (!token || token == "null") {
+    return res
+      .status(401)
+      .json({ success: false, message: "Not authorize to access this route" });
+  }
+
+  try {
+    //Verify Token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    console.log(decoded);
+
+    req.user = await User.findById(decoded.id);
+
+    next();
+  } catch (err) {
+    console.log(err.stack);
+    return res
+      .status(401)
+      .json({ success: false, message: "Not authorize to access this route" });
+  }
+};
---
+++ server.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const auth = require("./routes/auth");
+const cookieParser = require("cookie-parser");
+const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+const hpp = require("hpp");
+const cors = require("cors");
+
+// Load env vars
+dotenv.config({ path: "./config/config.env" });
+
+// Connect to database
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+
+//Sanitize Data
+app.use(mongoSanitize());
+
+app.use(xss());
+
+app.use(helmet());
+
+//Rate Limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 mins
+  max: 100,
+});
+
+app.use(limiter);
+
+app.use(hpp());
+
+app.use(cors());
+
+app.unsubscribe(cookieParser());
+
+app.use("/api/v1/auth", auth);
+
+app.get("/", (req, res) => {
+  res.status(200).json({ success: true, data: { id: 1 } });
+});
+
+const PORT = process.env.PORT || 3000;
+const server = app.listen(
+  PORT,
+  console.log(
+    "Server running in ",
+    process.env.NODE_ENV,
+    " mode on port ",
+    PORT
+  )
+);
+
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // close server & exit process
+  server.close(() => process.exit(1));
+});
